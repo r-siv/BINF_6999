d8941bdee8eabc559bf831205425aad9c4fe904d
r-siv <pokeraam510@gmail.com>
2022-07-13 03:40:55 -0400

    loop indents & simplified unitas annotation

--- analysis_log_6999.sh
+++ analysis_log_6999.sh
@@ -37,7 +37,10 @@ mkdir testing_data
 mv *.cat testing_data
 
 #check read counts for all test files
-for file in testing_data/*; do echo "$file"; grep "@" $file | wc -l; done
+for file in testing_data/*; 
+do 
+    echo "$file"; grep "@" $file | wc -l; 
+done
 #there are 9835594 reads in 01Ba_S13_R1_001.trim.cat
 #there are 3419851 reads in 01Bb_S6_R1_001.trim.cat
 #there are 2306411 reads in 24A_S44_R1_001.trim.cat
@@ -47,8 +50,14 @@ module spider seqtk
 module load seqtk
 
 #randomly downsample all files and check for correct read numbers
-for file in testing_data/*; do seqtk sample $file 10000 > $file.down.fastq; done
-for file in testing_data/*.fastq; do echo "$file"; grep "@" $file | wc -l; done
+for file in testing_data/*; 
+do 
+    seqtk sample $file 10000 > $file.down.fastq; 
+done
+for file in testing_data/*.fastq; 
+do 
+    echo "$file"; grep "@" $file | wc -l; 
+done
 #10000 reads found in all newly downsampled files
 
 #check and load fastqc module
@@ -58,7 +67,10 @@ module load fastqc
 #change directories and make new fastqc directory to perform preliminary fastqc statistics on all downsampled files. Move output into new directory
 cd ..
 mkdir fastqc_out
-for file in testing_data/*.fastq; do fastqc $file -o fastqc_out/; done
+for file in testing_data/*.fastq; 
+do 
+    fastqc $file -o fastqc_out/; 
+done
 
 #download html results from output directory onto current local machine directory for viewing
 #this is done from a new terminal on the local machine
@@ -76,62 +88,75 @@ pip install multiqc
 
 #generate multiqc reports from fastqc files into current directory
 multiqc fastqc_out/*
+deactivate
 
 #download html results from output directory onto current local machine directory for viewing
 #this is done from a new terminal on the local machine
 scp rsivakum@graham.computecanada.ca:~/scratch/6999/multiqc_report.html .
 
-#upload unitas, length filter, sRNAmapper, reallocate, proTrac, pingpong and repeatmasker perl scripts (obtained from https://www.smallrnagroup.uni-mainz.de/software.html) to 6999 directory on cluster
-#this is done from a new terminal on the local machine
-scp unitas_1.7.0.zip TBr2_length-filter.pl sRNAmapper.pl reallocate.pl proTRAC_2.4.2.pl TBr2_pingpong.pl RMvsMAP.pl rsivakum@graham.computecanada.ca:~/scratch/6999
+#make new directory called scripts for storing downstream scripts to be used
+mkdir scripts
 
-#unzip unitas script into current 6999 directory and change directories
-unzip unitas_1.7.0.zip
-cd unitas
+#upload unitas, length filter, sRNAmapper, reallocate, proTrac, pingpong and repeatmasker files (obtained from https://www.smallrnagroup.uni-mainz.de/software.html) to scripts directory on cluster
+#this is done from a new terminal on the local machine
+scp unitas_1.7.0.zip TBr2_length-filter.pl sRNAmapper.pl reallocate.pl proTRAC_2.4.2.pl TBr2_pingpong.pl RMvsMAP.pl rsivakum@graham.computecanada.ca:~/scratch/6999/scripts
+
+#unzip unitas script
+unzip scripts/unitas_1.7.0.zip
 
 #check and load perl module
 module spider perl
 module load perl
 
-#trim adaptors, collapse and filter low complexity reads (default filter at 75% repeats for a sequence) automatically on 01Ba file
-perl unitas_1.7.0.pl -i ~/scratch/6999/01Ba_S13_R1_001.trim.cat.downsampled.fastq -s bos_taurus -trim ?
-#####
-#NOTE: Fraction of reads containing putative adapter sequence is low!
-#Continue nevertheless.
-#Clip most abundant sequence (0.85%): TCCCCGACGGGG-(N)x-3'... OK.
-#Total processed reads: 10000
-#Clipped reads: 126
-#10-50 nt: 126
-#NOTE: Fraction of clipped reads is low!
-#####
-#trim adaptors, collapse and filter low complexity reads automatically on 01Bb file
-perl unitas_1.7.0.pl -i ~/scratch/6999/01Bb_S6_R1_001.trim.cat.downsampled.fastq -s bos_taurus -trim ?
-#####
-#NOTE: Fraction of reads containing putative adapter sequence is low!
-#Continue nevertheless.
-#Clip most abundant sequence (2.15%): TAGATCCGAACT-(N)x-3'... OK.
-#Total processed reads: 10000
-#Clipped reads: 0
-#10-50 nt: 0
-#NOTE: Fraction of clipped reads is low!
-#####
-
+#collapse and filter low complexity reads (default filter at 75% repeats for a sequence) automatically on all files
+for file in testing_data/*.fastq; 
+do 
+    perl unitas/unitas_1.7.0.pl -i $file -s bos_taurus; 
+done
 #note when using -trim ? option, different files are created so unsure if it should be performed (lines 117 & 128)
 
-#count number of annotated piRNA sequences from 01Bb file (#2 is untrimmed file version)
-grep ">" UNITAS_10-04-2022_01Bb_S13_R1_001.trim.cat.downsampled.fastq_#2/fasta/unitas.piRcandidates.fas | wc -l
-#there are 1600 annotated reads
-#count number of unannotated reads from this file
-grep ">" UNITAS_10-04-2022_01Bb_S13_R1_001.trim.cat.downsampled.fastq_#2/fasta/unitas.no-annotation.fas | wc -l
-#there are 3742 unannotated reads
-#count number of annotated piRNA sequences from 01Ba file (#2 is untrimmed file version)
-grep ">" UNITAS_10-04-2022_01Ba_S13_R1_001.trim.cat.downsampled.fastq_#2/fasta/unitas.piRcandidates.fas | wc -l
-#there are 1021 annotated reads
-#count number of unannotated reads from this file
-grep ">" UNITAS_10-04-2022_01Ba_S13_R1_001.trim.cat.downsampled.fastq_#2/fasta/unitas.no-annotation.fas | wc -l
-#there are 5473 unannotated reads
+#for 01Ba_S13_R1_001.trim.cat.down.fastq 9893 sequences are ok, 107 sequences are dusty (low-complexity)
+#  Monomer:             17
+#  Biased for 2 bases:  61
+#  2 nt motif enriched: 3
+#  3 nt motif enriched: 8
+#  4 nt motif enriched: 1
+#  5 nt motif enriched: 3
+#for 01Bb_S6_R1_001.trim.cat.down.fastq 9930 sequences are ok, 70 sequences are dusty (low-complexity)
+#  Monomer:             8
+#  Biased for 2 bases:  50
+#  2 nt motif enriched: 1
+#  3 nt motif enriched: 5
+#  4 nt motif enriched: 2
+#  5 nt motif enriched: 1
+#for 24A_S44_R1_001.trim.cat.down.fastq 9789 sequences are ok, 211 sequences are dusty (low-complexity)
+#  Monomer:             43
+#  Biased for 2 bases:  113
+#  2 nt motif enriched: 1
+#  3 nt motif enriched: 7
+#  4 nt motif enriched: 1
+#  5 nt motif enriched: 4
 
-#download whole folder to display html results from both 01Ba and 01Bb directories onto current local machine directory
+#count number of annotated piRNA sequences from each file
+for group in *\#1; 
+do 
+    echo $group; grep ">" $group/fasta/unitas.piRcandidates.fas | wc -l; 
+done
+#there are 1021 piRNA reads in 01Ba_S13_R1_001.trim.cat
+#there are 1600 piRNA reads in 01Bb_S6_R1_001.trim.cat
+#there are 1806 piRNA reads in 24A_S44_R1_001.trim.cat
+
+#count number of unannotated reads from each file
+for group in *\#1; 
+do 
+    echo $group; grep ">" $group/fasta/unitas.no-annotation.fas | wc -l; 
+done
+#there are 5473 unannotated reads in 01Ba_S13_R1_001.trim.cat
+#there are 3742 unannotated reads in 01Bb_S6_R1_001.trim.cat
+#there are 4560 unannotated reads in 24A_S44_R1_001.trim.cat
+
+#download all Unitas folders to display html results onto current local machine directory
 #this is done from a new terminal on the local machine
-scp -r rsivakum@graham.computecanada.ca:~/scratch/6999/unitas/UNITAS_10-04-2022_01Bb_S6_R1_001.trim.cat.downsampled.fastq_#2 .
-scp -r rsivakum@graham.computecanada.ca:~/scratch/6999/unitas/UNITAS_10-04-2022_01Ba_S13_R1_001.trim.cat.downsampled.fastq_#2 .
+scp -r rsivakum@graham.computecanada.ca:~/scratch/6999/*\#1 .
+
+
